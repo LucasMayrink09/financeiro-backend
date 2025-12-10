f411d6b56eb7bd445d6544a5e72255fa856d995f
Lucas Mayrink Oliveira <lucas.mayrink@zerohora.com.br>
2025-12-10 12:09:26 -0300

    return saldo repository

--- src/main/java/com/gestao/financeira/repository/SaldoRepository.java
+++ src/main/java/com/gestao/financeira/repository/SaldoRepository.java
@@ -2,12 +2,7 @@ package com.gestao.financeira.repository;
 
 
 import com.gestao.financeira.entity.Saldo;
-import com.gestao.financeira.entity.User;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SaldoRepository extends JpaRepository<Saldo, Long> {
-    boolean existsByUserId(Long userId);
-    Page<Saldo> findByUser(User user, Pageable pageable);
 }
